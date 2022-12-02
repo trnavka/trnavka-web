1684c0427a2c5774df8c21853f06b97de5bdd7f8
“Filip <filip@bratia.sk>
2022-12-02 10:11:50 +0100

    Change "Why donate" text

--- packages/trnavka-theme/resources/views/partials/campaign/donate.blade.php
+++ packages/trnavka-theme/resources/views/partials/campaign/donate.blade.php
@@ -6,8 +6,14 @@
             </div>
             <div class="col-md-6">
                 <div class="text-content">
-                    <h2>Prečo sa zapojiť?</h2>
-                    <p>TRNÁVKA - saleziánske dielo je o stovkách ľudí, ktorí tu trávia zmysluplný čas. Tvoj pravidelný finančný príspevok umožní jednotlivým zložkám diela skvalitňovať to, čomu sa venujú. Tvoj príspevok je motorom pre udržanie a rozvoj duchovných, sociálnych aj športových aktivít pre deti, mladých, rodičov aj seniorov.</p>
+                    <h2>Prečo začať prispievať?</h2>
+                    <p>
+                        <strong>TRNÁVKA - saleziánske dielo</strong> je komunitné centrum, v ktorom stovky ľudí denne trávia zmysluplný čas.
+                        Širokú ponuku služieb využívajú ľudia z Trnávky a širšieho okolia bez ohľadu na vierovyznanie. Každý je vítaný.
+                        Tvoj pravidelný mesačný príspevok je potrebný pre udržanie a rozvoj športových, kultúrnych, sociálnych a duchovných aktivít pre deti, mladých, rodičov aj seniorov.
+
+{{--                        TRNÁVKA - saleziánske dielo je o stovkách ľudí, ktorí tu trávia zmysluplný čas. Tvoj pravidelný finančný príspevok umožní jednotlivým zložkám diela skvalitňovať to, čomu sa venujú. Tvoj príspevok je motorom pre udržanie a rozvoj duchovných, sociálnych aj športových aktivít pre deti, mladých, rodičov aj seniorov.--}}
+                    </p>
                 </div>
             </div>
         </div>
