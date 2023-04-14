db251bbefb092525cd2cb6a7ed8910d1af769096
“Filip <filip@bratia.sk>
2023-04-14 10:01:13 +0200

    Add Daj na to button to header for small screen

--- packages/trnavka-theme/resources/views/partials/campaign/header.blade.php
+++ packages/trnavka-theme/resources/views/partials/campaign/header.blade.php
@@ -4,9 +4,13 @@
             <h1>Daj na to!</h1>
             <div class="row">
                 <div class="col-md-6">
-                    <p class="lead ">
+                    <p class="lead">
                         Tvoj pravidelný dar dá impulz tomu, na&nbsp;čom pracujeme.
                     </p>
+
+                    <div class="header-dajnato-button">
+                        <button type="button" class="btn-donate" data-bs-toggle="modal" data-bs-target="#donationModal">Daruj teraz</button>
+                    </div>
                 </div>
             </div>
         </div>
